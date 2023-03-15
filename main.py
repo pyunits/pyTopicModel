475114c41b1a2d094258527a05117cbabd6c9af9
jtyoui <jtyoui@qq.com>
2023-03-15 11:32:16 +0800

    增加参数解析

--- main.py
+++ main.py
@@ -6,25 +6,48 @@ from topicModel import TopicModel, preprocess_func, Config
 import json
 import os
 import argparse
+import sys
 
 config = Config()
-parser = argparse.ArgumentParser(description='输入主题模型需要的参数')
 
-config.AbstractColumName = "abstract"
-config.TitleColumNane = "title"
-config.IDColumName = "ID"
 
-dirs = os.path.dirname(__file__)
-config.SourceDir = os.path.join(dirs, "data")
-config.DataPath = os.path.join(config.SourceDir, "基金—呼吸.xlsx")
+def parse_arg(argv):
+    parser = argparse.ArgumentParser(description='输入主题模型需要的参数', usage="主题模型")
 
-config.StopPath = os.path.join(dirs, "stopwords.txt")
-config.ModelDir = os.path.join(config.SourceDir, "model")
-config.PreprocessPath = os.path.join(config.SourceDir, "preprocess_words.txt")
-config.WordPath = os.path.join(config.SourceDir, "word_frequency.xlsx")
-config.LdaPath = os.path.join(config.SourceDir, "doc_lda.xlsx")
-config.TopicsRange = (10, 100)
-Config.k = 135
+    parser.add_argument("mode", choices=["train", "predict"], help="选择程序模式", default="train")
+    parser.add_argument("-p", type=str, help="加载分析的数据路径", required=True)
+    parser.add_argument("-a", type=str, help="摘要列名", default="abstract", required=False)
+    parser.add_argument("-t", type=str, help="标题列名", default="title", required=False)
+    parser.add_argument("-d", type=str, help="保存结果的文件夹", default="./result", required=False)
+    parser.add_argument("-r", nargs=2, type=int, help="主题数取值范围：至少是1", default=[1, 50], required=False)
+    parser.add_argument("-k", type=int, help="预测主题数", default=1, required=False)
+    return parser.parse_args(argv)
+
+
+def main(args):
+    print(args)
+    config.AbstractColumName = args.a
+    config.TitleColumNane = args.t
+    config.TopicsRange = args.r
+    config.K = args.k
+
+    dirs = os.path.abspath(args.d)
+    config.DataPath = os.path.abspath(args.p)
+
+    config.StopPath = os.path.join(dirs, "stopwords.txt")
+    config.ModelDir = os.path.join(dirs, "model")
+    config.PreprocessPath = os.path.join(dirs, "preprocess_words.txt")
+    config.WordPath = os.path.join(dirs, "word_frequency.xlsx")
+    config.PredictPath = os.path.join(dirs, "doc_lda.xlsx")
+
+    if args.mode == 'train':
+        print('进入训练模式')
+        train()
+    elif args.mode == 'predict':
+        print('进入测试模式')
+        predict()
+    else:
+        raise Exception("第一个参数错误：选择 train 和 predict")
 
 
 # 训练
@@ -45,8 +68,8 @@ def predict():
     with open(config.PreprocessPath, encoding="utf-8") as fp:
         content = json.load(fp)
     model = TopicModel(config.TopicsRange, content, config.ModelDir)
-    model.exec_lda(config.K, config.LdaPath)
+    model.exec_lda(config.K, config.PredictPath)
 
 
 if __name__ == '__main__':
-    train()  # predict()
+    main(parse_arg(sys.argv[1:]))
--- topicModel/config.py
+++ topicModel/config.py
@@ -4,16 +4,13 @@
 # @Email: jtyoui@qq.com
 
 class Config:
-    TopicsRange: (int, int)  #
+    TopicsRange: (int, int)  # 主题范围
     PreprocessPath: str  # 预测文件路径
-    SourceDir: str  #
-    IDColumName: str
-    AbstractColumName: str  #
-    TitleColumNane: str  #
+    AbstractColumName: str  # 摘要列名
+    TitleColumNane: str  # 标题列名
     DataPath: str  # 加载数据源的文本路径
     StopPath: str  # 停用词文件路径
-    ModelDir: str  #
+    ModelDir: str  # 模型保存的路径
     WordPath: str  # 词频文件路径
-    LdaPath: str  #
-    Name: str  #
-    K: int  #
+    PredictPath: str  # 预测出来结果保存的路径
+    K: int  # 主题数
